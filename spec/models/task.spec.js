cfc82cd7ccfab80cd5a7506f0c7824cc2d7d9486
Dmitry Rocha <dmitryrck@gmail.com>
2015-05-24 17:38:24 -0300

    Use jasmine instead of jasmine-node and avoid use of "done" to specs.

--- spec/models/task.spec.js
+++ spec/models/task.spec.js
@@ -1,24 +1,21 @@
 describe("Task", function() {
   var Task = global.db.task;
 
-  beforeEach(function(done) {
+  beforeEach(function() {
     Task.sync();
-    done();
   });
 
-  afterEach(function(done) {
+  afterEach(function() {
     Task.destroy({ truncate: true });
-    done();
   });
 
-  it("create", function(done) {
+  it("create", function() {
     Task.create({ title: 'a title' }).then(function(task) {
       expect(task.title).toBe('a title');
-      done();
     });
   });
 
-  it("create again", function(done) {
+  it("create again", function() {
     this.complete = { title: 'Task#1', complete: true };
     this.incomplete = { title: 'Task#2', complete: false };
 
@@ -28,7 +25,6 @@ describe("Task", function() {
     ]).then(function() {
       Task.scope('active').findAll().then(function(tasks) {
         expect(tasks.length).toBe(1);
-        done();
       });
     });
   });
